50142a2c48b6f5d4b7c33e1fa6d9241a29b447d4
Jenny <tgfjr.jenny@gmail.com>
2024-01-04 09:48:44 +0900

    edit_0104

--- js/script.js
+++ js/script.js
@@ -5,4 +5,36 @@ $(document).ready(function(){
     $(".gnbBtn_close").click(function(){
         $(".gnb_hidden").removeClass("active")
     })
+
+
+    let train = $(".banner_imgList")
+    let trainLi = train.children()
+    let trainLength = trainLi.length
+    
+    let des =  $(".banner_desList")
+    let desLi = des.children()
+
+    let count = 0
+
+
+    
+    let stopSlide = setInterval(function(){
+        count++
+        if(count>(trainLength - 1)){count=0}
+        trainLi.removeClass("on")
+        trainLi.eq(count).addClass("on")
+
+        desLi.removeClass("on")
+        desLi.eq(count).addClass("on")
+    }, 4200)
+
+    trainLi.click(function(){
+        clearInterval(stopSlide)
+        let idx = $(this)
+        trainLi.removeClass("on")
+        trainLi.eq(this).addClass("on")
+        desLi.removeClass("on")
+        desLi.eq(idx).addClass("on")
+        // clearInterval(stopSlide)
+    })
 })
