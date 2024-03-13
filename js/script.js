e3c032e796f0953a5bb3d0a2a95a47c4513ff95a
Yeong Hwan Kim <kch8317@gmail.com>
2024-03-14 02:29:07 +0900

    edit

--- js/script.js
+++ js/script.js
@@ -18,15 +18,15 @@ $(document).ready(function(){
 
 
     
-    // let stopSlide = setInterval(function(){
-    //     count++
-    //     if(count>(trainLength - 1)){count=0}
-    //     trainLi.removeClass("on")
-    //     trainLi.eq(count).addClass("on")
+    let stopSlide = setInterval(function(){
+        count++
+        if(count>(trainLength - 1)){count=0}
+        trainLi.removeClass("on")
+        trainLi.eq(count).addClass("on")
 
-    //     desLi.removeClass("on")
-    //     desLi.eq(count).addClass("on")
-    // }, 7000)
+        desLi.removeClass("on")
+        desLi.eq(count).addClass("on")
+    }, 7000)
 
     // trainLi.mouseover(function(){
     //     clearInterval(stopSlide)
